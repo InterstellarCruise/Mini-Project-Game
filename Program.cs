cef1d0214f8a1b42bff8deeae736e01f6885eebf
AngelovdBroek <1055539@hr.nl>
2023-02-16 15:48:34 +0100

    Description added and quit added

--- Program.cs
+++ Program.cs
@@ -5,16 +5,24 @@
     {
         int loc = 0;
         bool game = true;
-        while (game)
+        ConsoleKey walk = Console.ReadKey().Key;
+        while (walk != ConsoleKey.Q)
         {
             Console.WriteLine("Press W to move");
-            ConsoleKey walk = Console.ReadKey().Key;
+            walk = Console.ReadKey().Key;
+            Console.WriteLine("");
             if (walk == ConsoleKey.W)
             {
                 loc++;
             }
+            if (walk == ConsoleKey.Q)
+            {
+                Console.WriteLine("You have quit the game!");
+            }
+
             Location LocationObject = World.LocationByID(loc);
             Console.WriteLine(LocationObject.Name);
+            Console.WriteLine(LocationObject.Description);
             Console.WriteLine(((LocationObject.LocationToNorth != null) ? LocationObject.LocationToNorth.Name : (LocationObject.LocationToEast != null) ? LocationObject.LocationToEast.Name : (LocationObject.LocationToSouth != null) ? LocationObject.LocationToSouth.Name : (LocationObject.LocationToWest != null) ? LocationObject.LocationToWest.Name : "No Locations nearby"));
 
 
