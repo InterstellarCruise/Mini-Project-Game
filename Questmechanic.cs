c2d2ad7eb03247af47eae2d6818ec68add4177cd
wesley7886 <124694992+wesley7886@users.noreply.github.com>
2023-02-22 16:02:13 +0100

    Add files via upload

---
+++ Questmechanic.cs
@@ -0,0 +1,44 @@
+public class Questmechanic
+{
+    public Location Location;
+
+    public Questmechanic(Location location)
+    {
+
+        this.Location = location;
+    }
+
+
+    public void Start()
+    {
+        if(Location.QuestAvailableHere is not null){
+                    int number = Convert.ToInt32(Location.QuestAvailableHere);
+                    Quest questObject = World.QuestByID(number);
+                    Item itemObject = World.ItemByID((number*2));
+                    Console.WriteLine(questObject.QuestName);
+                    Console.WriteLine(questObject.QuestDescription);
+                    Console.WriteLine("do you wish to accept this qeust y/n");
+                    string? answer = Console.ReadLine().ToUpper();
+                    if(answer == "Y"){
+                        Console.Write(" great get to it then ");
+                        QuestList.QuestLog.Add(questObject);
+                        
+                        }
+                    
+                    if(answer != "Y"){
+                        while(answer != "Y"){
+                        Console.Write("NO, but i need a YES TO CONTINUE so ANSWER the question again :)");
+                        answer = Console.ReadLine();
+                        if(answer == "Q"){
+                            break;
+                        }
+                    
+                    if(answer == "Q"){
+                            break;
+                        }
+                        }
+        }
+    }
+}
+}
+
